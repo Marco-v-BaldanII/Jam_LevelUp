c9f19e713d847009656b9860b33bf44366a45339
VM Marco <marcoxativa@gmail.com>
2024-07-02 19:31:53 +0200

    wolf_movement

---
+++ Assets/Scripts/CottonMines.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CottonMines : wolf_task
+{
+    public float mining_time = 5.0f;
+    private Wolf_AI mining_wolf;
+    int num_cotton = 0;
+
+    private bool mining = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+        if (my_wolfs.Count() <= 0) { mining = false; }
+
+        if (mining == false && num_wolves > 0)
+        {
+            mining = true;
+            StartCoroutine("Mine_Cotton");
+
+        }
+
+    }
+
+    private IEnumerator Mine_Cotton()
+    {
+        yield return new WaitForSeconds(3);
+
+        while (num_wolves > 0)
+        {
+            int w_index = 0;
+            while (my_wolfs[w_index % num_wolves].has_cotton == true) { w_index++; yield return null; if (num_wolves <= 0) { break; } }
+            if (num_wolves <= 0) { break; }
+                my_wolfs[w_index % num_wolves].has_cotton = true; // Give Cotton to cottonless wolf
+            
+ 
+
+        }
+
+
+    }
+    public override void OnTriggerExit2D(Collider2D collision)
+    {
+        base.OnTriggerExit2D(collision);
+    }
+
+}
---
+++ Assets/Scripts/Wolf_AI.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf_AI : MonoBehaviour
+{
+    public int movement_speed = 3;
+
+    private bool _isDragging;
+
+    public wolf_task my_task;
+    public GameObject wolf_city;
+    public GameObject marshmallow;
+    private Vector3 buffer_pos;
+    private Rigidbody2D rigid;
+
+    public bool moving_towards_task = false;
+    public bool moving_towards_base = false;
+
+    public bool has_cotton = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (moving_towards_task && my_task != null)
+        {
+            direction =  my_task.transform.position - transform.position;
+
+
+        }
+        else if (has_cotton && my_task != null)
+        {
+            direction = wolf_city.transform.position - transform.position;
+        }
+
+        rigid.velocity = direction.normalized * movement_speed;
+
+        Debug.Log(has_cotton != marshmallow.activeSelf);
+        if (has_cotton != marshmallow.activeSelf)
+        {
+            marshmallow.SetActive(has_cotton);
+        }
+
+    }
+
+    public void OnMouseDrag()
+    {
+        _isDragging = true;
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+    
+    }
+
+    private void OnMouseUp()
+    {
+        transform.position = buffer_pos;
+        _isDragging = false;
+    }
+
+    public void OnMouseEnter()
+    {
+        // Check that they are idle
+        buffer_pos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Task") == true && _isDragging == false )
+        {
+            moving_towards_task = false;
+        }
+    }
+
+}

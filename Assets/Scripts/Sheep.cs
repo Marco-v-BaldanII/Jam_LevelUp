1f8fac1406d346277e3bf80c9c90a817b6db0da7
VM Marco <marcoxativa@gmail.com>
2024-07-03 11:57:32 +0200

    sheep horde

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Sheep : MonoBehaviour
 {
     public float moveSpeed = 5;
-    public Vector3 destination;
+    public Transform destination;
     public int damage;
     public bool angered;
     public int life;
@@ -13,10 +13,11 @@ public class Sheep : MonoBehaviour
 
     private bool shouldMove = true;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        destination = new Vector2(10, 0);
+       // destination = new Vector2(10, 0);
         dead = false;
         angered = false;
         life = 2;
@@ -48,10 +49,10 @@ public class Sheep : MonoBehaviour
     {
         // Sheep moves to the designated destination
         //transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
-        transform.position = Vector2.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, destination.position, moveSpeed * Time.deltaTime);
 
         // Checks if the sheep has arrived to the destination
-        if (Vector2.Distance(transform.position, destination) < 0.1f)
+        if (Vector2.Distance(transform.position, destination.position) < 0.1f)
         {
             //Debug.Log("Sheep has arrived the destination");
             shouldMove = false;
--- Assets/Scripts/SheepSpawnerScript.cs
+++ Assets/Scripts/SheepSpawnerScript.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum DIFFICULTY
+{
+    EASY,
+    MEDIUM,
+    HARD,
+    HERALD_OF_CHAOS
+
+}
 
 public class SheepSpawnerScript : MonoBehaviour
 {
     public GameObject sheepPrefab;
     public float spawnRate = 2f;
+    public Transform city_destination;
+    public Wolf_City city;
+    public TextMeshProUGUI sheep_warning;
     private float timer = 0f;
+    private float global_timer = 0f;
+
+    private DIFFICULTY sheep_difficulty = DIFFICULTY.HERALD_OF_CHAOS;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnSheep();
+        StartCoroutine("Spawning_Sheep_Horde");
+      
     }
 
     // Update is called once per frame
@@ -20,7 +38,7 @@ public class SheepSpawnerScript : MonoBehaviour
         timer += Time.deltaTime;
         if (timer >= spawnRate)
         {
-            SpawnSheep();
+           // SpawnSheep();
             timer = 0f;
         }
     }
@@ -29,6 +47,54 @@ public class SheepSpawnerScript : MonoBehaviour
     {
         GameObject newSheep = Instantiate(sheepPrefab, transform.position, transform.rotation);
         Sheep sheepScript = newSheep.GetComponent<Sheep>();
+        sheepScript.destination = city_destination;
       
     }
+
+    private IEnumerator Spawning_Sheep_Horde()
+    {
+        while(city.intelligence_bar.Get() > -1)
+        {
+            float wait_time = 0.0f;
+            switch (sheep_difficulty)
+            {
+                case DIFFICULTY.EASY:
+                    wait_time = Random.Range(30.0f, 45.0f);
+                    break;
+                case DIFFICULTY.MEDIUM:
+                    wait_time = Random.Range(25.0f, 35.0f);
+                    break;
+                case DIFFICULTY.HARD:
+                    wait_time = Random.Range(18.0f, 28.0f);
+                    break;
+                case DIFFICULTY.HERALD_OF_CHAOS:
+                    wait_time = Random.Range(12.0f, 22.0f);
+                    break;
+
+
+            }
+
+
+            yield return new WaitForSecondsRealtime(wait_time-3);
+
+            sheep_warning.gameObject.SetActive(true);
+
+            yield return new WaitForSecondsRealtime(3);
+
+            sheep_warning.gameObject.SetActive(false);
+
+            int spawn_amount = Random.Range(3, 6);
+            for(int i = 0; i < spawn_amount; ++i)
+            {
+                SpawnSheep();
+                yield return new WaitForSecondsRealtime(Random.Range(0.3f, 1.2f));
+
+
+            }
+
+        }
+
+
+    }
+
 }
